b9a2ac0c30611db9b509327300b86840ab844dbb
Luca Mock <luca.mock@ringieraxelspringer.ch>
2022-12-08 09:58:33 +0100

    first commit

---
+++ src/main/java/bbw/ch/Morning/MorningApplication.java
@@ -0,0 +1,13 @@
+package bbw.ch.Morning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bbw/ch/Morning/MorningApplicationTests.java
@@ -0,0 +1,13 @@
+package bbw.ch.Morning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
