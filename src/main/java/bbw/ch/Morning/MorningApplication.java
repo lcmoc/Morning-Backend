577aba10f77772ad9055574b33f23e7e013d8369
Canmaya <kuebra.canmaya@outlook.com>
2022-12-19 16:15:32 +0100

    created database

--- src/main/java/bbw/ch/Morning/MorningApplication.java
+++ src/main/java/bbw/ch/Morning/MorningApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MorningApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MorningApplication.class, args);
 	}
-
 }
