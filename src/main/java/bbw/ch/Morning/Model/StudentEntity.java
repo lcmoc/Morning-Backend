0ed9dc8694c38c00ba5ed4166345994ae71ad565
Canmaya <kuebra.canmaya@outlook.com>
2022-12-21 21:21:27 +0100

    updated student en

--- src/main/java/bbw/ch/Morning/Model/StudentEntity.java
+++ src/main/java/bbw/ch/Morning/Model/StudentEntity.java
@@ -21,6 +21,30 @@ public class StudentEntity implements Serializable {
     @Column(name = "password", nullable = false)
     private String password;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public StudentEntity() {
     }
 }
