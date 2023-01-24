98452a85a60813410631adad6ff0f5ef3ae23328
lcmoc <luca.mock@ringieraxelspringer.ch>
2023-01-24 11:40:05 +0100

    added flyway to project & created migration files

---
+++ src/main/resources/db/migration/V1_createTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `student` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `email` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_0900_ai_ci
